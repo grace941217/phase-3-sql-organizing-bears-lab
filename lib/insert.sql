b995b5a52504d4778c5fb6777d34cf666af7be62
Grace <grace941217@gmail.com>
2022-11-03 17:53:35 -0400

    commit tree d7027cf649d48d548d9e1218b62a621d99b677c5

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES 
+    ("Mr. Chocolate", 2, "M", "red", "calm", "Yes"),
+    ("Rowdy", 3, "F", "orange", "calm", "Yes"),
+    ("Tabitha", 4, "M", "yellow", "calm", "Yes"),
+    ("Sergeant Brown", 5, "F", "green", "calm", "Yes"),
+    ("Melissa", 6, "M", "blue", "calm", "Yes"),
+    ("Grinch", 7, "F", "indigo", "calm", "Yes"),
+    ("Wendy", 8, "M", "violet", "calm", "Yes"),
+    (NULL, 9, "F", "black", "calm", "Yes");
